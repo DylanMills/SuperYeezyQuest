31981637b5353c0457dc41ad032396a223e9dae0
Dylan <dylanmills@rogers.com>
2021-10-16 14:38:53 -0400

    Updated Gameboard/Tiles
    Updated Gameboard and tiles. Scaled the board to match the correct movement distance. Updated the knights pathing.

--- BoardGame/Assets/Scripts/Board.cs
+++ BoardGame/Assets/Scripts/Board.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [System.Serializable]
 public class Board : MonoBehaviour
 {
-    private Vector2[] _tilePositions = new Vector2[89];
+    private Vector2[] _tilePositions = new Vector2[88];
     private Vector2 _default = new Vector2(8.5f, -6.5f);
 
     public Vector2[] GetTilePositions()
@@ -16,7 +16,7 @@ public class Board : MonoBehaviour
         int n = 0;
         Vector2 temp = new Tile(_default).Vec();
         _tilePositions[n] = temp;
-        for (n = 1; n <= 88; n++)
+        for (n = 1; n <= 87; n++)
         {
             _tilePositions[n] = new Tile(_tilePositions[n - 1]).Vec();
         }
@@ -33,36 +33,21 @@ public class Board : MonoBehaviour
             id++;
             prev = previous;
 
-            if (id == 5 || id == 11 || id == 75 || id == 76 || id == 84 || id == 85)
-            {
-                current = prev + new Vector2(0f, 1f); //north
-            }
-            else if (id == 24 || id == 25 || id == 26)
-            {
-                current = prev + new Vector2(1f, 0f); //east
-            }
-            else if (id == 49 || id == 50 || id == 57 || id == 58 || id == 63 || id == 64 || id == 69 
-                            || id == 70 || id == 71 || id == 72 || id == 79 || id == 80 || id == 81)
-            {
-                current = prev + new Vector2(0f, -1f); //south 49 50 57 58 63 64 69 70 71 72 79 80 81
-            }
-            else if (id == 30 || id == 31 || id == 32 || id == 33 || id == 34 || id == 59 || id == 60 
-                             || id == 61 || id == 62 || id == 73 || id == 74 || id == 77 || id == 78 
-                             || id == 82 || id == 83 || id >85)
-            {
-                current = prev + new Vector2(-1f, 0f); //west 30-34 59 - 62, 73 74, 77,78,82,83,86,87,88
-            }
-            else if (id >= 1 && id <= 17)
+            if ((id >= 1 && id <= 7) || (id >= 10 && id <= 15) || (id >= 19 && id <= 22) || (id >= 43 && id <= 48) || (id >= 68 && id <= 71) || (id >= 82))
             {
                 current = prev + new Vector2(-1f, 0f); //west
             }
-            else if (id >= 18 && id <= 38)
+            else if ((id >= 8 && id <= 9) || (id >= 23 && id <= 32) || (id >= 40 && id <= 42) || (id >= 49 && id <= 50))
             {
                 current = prev + new Vector2(0f, 1f); //north
             }
-            else if (id >= 39 && id <= 68)
+            else if ((id >= 33 && id <= 39) || (id >= 51 && id <= 64) || (id >= 75 && id <= 78))
             {
                 current = prev + new Vector2(1f, 0f); //east
+            }
+            else
+            {
+                current = prev + new Vector2(0f, -1f); //south
             }
         }
 
